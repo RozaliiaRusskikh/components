b6f649adbd3932a49069f3790f7a3db7710de866
RozaliiaRusskikh <rozaliiarusskikh@Rozaliias-MacBook-Air.local>
2022-12-14 12:56:30 -0800

    Added icons

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import Button from "./Button";
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go'
 
 function App() {
+
+    const handleClick = () => {
+        console.log('Click!');
+    }
+
     return <div>
         <div>
-            <Button success rounded outline>Click Me!</Button>
+            <Button success rounded outline onClick={handleClick}>
+                <GoBell />
+                Click Me!
+            </Button>
         </div>
         <div>
             <Button danger outline>Buy Now</Button>
         </div>
         <div>
-            <Button warning>See Deal</Button>
+            <Button warning>
+                <GoCloudDownload />
+                See Deal
+            </Button>
         </div>
         <div>
             <Button secondary outline>Hide Ads</Button>
         </div>
         <div>
-            <Button secondary rounded>Something</Button>
+            <Button primary rounded>
+                <GoDatabase />
+                Something
+            </Button>
         </div>
     </div>
 }
