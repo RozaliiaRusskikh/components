e8f99c0ed98fdaa9cf6aa45f0905ac48932e9ded
RozaliiaRusskikh <rozaliiarusskikh@Rozaliias-MacBook-Air.local>
2022-12-16 20:22:21 -0800

    Dropdown  added1

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
     return (
         <div>
             <label> Select a Color</label>
-            <Dropdown options={options} selection={selection} onSelect={handleSelect} />
+            <Dropdown options={options} value={selection} onChange={handleSelect} />
         </div>
     )
 }
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
+import { GoChevronDown } from 'react-icons/go'
 
-function Dropdown({ options, selection, onSelect }) {
+function Dropdown({ options, value, onChange }) {
     const [isOpen, setIsOpen] = useState(false);
     const handleClick = () => {
         setIsOpen(!isOpen);
@@ -10,22 +11,26 @@ function Dropdown({ options, selection, onSelect }) {
         //CLOSE DROPDOWN
         setIsOpen(false);
         // ACTIVE OPTION
-        onSelect(option)
+        onChange(option)
     }
 
     const renderedDropdownItems = options.map((item) => {
-        return <div onClick={() => handleOptionClick(item)} key={item.value}>{item.label}</div>
+        return <div className='hover:bg-sky-100 rounded cursor-pointer p-1' onClick={() => handleOptionClick(item)} key={item.value}>{item.label}</div>
     })
 
     let content = 'Select...';
-    if(selection) {
-        content = selection.label
+    if (value) {
+        content = value.label
     }
 
-    return <div>
-        <div onClick={handleClick}>{content}</div>
-        {isOpen && <div>{renderedDropdownItems}</div>}
-    </div>
+    return (
+        <div className='w-48 relative'>
+            <div className='flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full' onClick={handleClick}>{content}
+                <GoChevronDown />
+            </div>
+            {isOpen && <div className='absolute top-full border rounded p-3 shadow bg-white w-full'>{renderedDropdownItems}</div>}
+        </div>
+    )
 }
 
 export default Dropdown;
