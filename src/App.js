87eda65e2da6fc64d72ee4fb7100d2ae05706609
RozaliiaRusskikh <rozaliiarusskikh@Rozaliias-MacBook-Air.local>
2022-12-21 14:43:33 -0800

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Route from './components/Route';
+import AccordionPage from './pages/AccordionPage'
+import DropdownPage from './pages/DropdownPage'
+import SideBar from './components/SideBar'
+import ButtonPage from './pages/ButtonPage'
+
 function App() {
-    return <div></div>
+    return <div>
+        <SideBar />
+        <div>
+            <Route path='/accordion' > <AccordionPage /></Route>
+            <Route path='/' > <DropdownPage /></Route>
+            <Route path='/buttons' > <ButtonPage /></Route>
+        </div>
+    </div>
 }
 
 export default App;
